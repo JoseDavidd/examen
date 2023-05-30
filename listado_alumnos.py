b052b49b2b298b173ee16277595bb4308fd2cd1b
Jose David <Jd>
2023-05-30 11:30:16 +0200

    Listado de alumnos

---
+++ listado_alumnos.py
@@ -0,0 +1,5 @@
+alumnos ={
+{"id":1,"Nombre":"Juan pérez", "Edad":18, -"Curso":12},
+{"id":2,"Nombre": "María López", "Edad":17,"Curso":11},
+{"id":3,"Nombre": "Pedro Gómez","Edad":19,"Curso":12}
+}
